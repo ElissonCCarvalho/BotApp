99d7000eff6866b853061a275238c02da1e8c210
Elisson <elissoncarvalho314@gmail.com>
2023-11-29 21:18:39 -0300

    add ChooseBot layout

---
+++ app/src/main/java/com/botapp/view/ChooseBotActivity.kt
@@ -0,0 +1,15 @@
+package com.botapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.botapp.R
+
+class ChooseBotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_bot)
+
+
+    }
+}
